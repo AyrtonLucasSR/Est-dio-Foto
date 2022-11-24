05cade88f96c00373ac4f3a93d5ad102da3322d0
Ayrton Lucas <ayrtondesigns@hotmail.com>
2022-11-24 10:28:02 -0300

    Implementação do componente menu2

---
+++ menu2.js
@@ -0,0 +1,10 @@
+function criarMenu(menu) {
+	
+	for (const m of menus) {
+		var manu = document.createElement('menu');
+		var ul = document.createElement('ul');
+		var item1 = document.createElement('li');
+		var item2 = document.createElement('li');
+		item1.innerHTML = 'Home';
+		item2.innetHTML = 'Produtos';
+		
